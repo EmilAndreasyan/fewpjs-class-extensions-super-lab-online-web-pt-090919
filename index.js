ee1f21e812ad0aa57bc882562f2b09638ecd4922
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-22 05:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ class Evergreen extends Tree {
   }
   static definition(){
     return (
-      super.definition() + 
+      super.definition() + `Evergreens keep their leaves all year round`
       )
   }
 }
